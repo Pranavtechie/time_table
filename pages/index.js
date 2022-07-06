a21a459d09924901762b994472e956e5b1e6eb50
Pranav <pranav.techiegeek@gmail.com>
2022-07-06 17:33:52 +0530

    basic setup done

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-6xl text-center text-blue-500">Time Table</h1>
+    </div>
+  );
+}
