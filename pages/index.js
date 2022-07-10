69f9c13db8505334d586267cf92ea0d0db7e6ddd
Pranav <pranav.techiegeek@gmail.com>
2022-07-11 01:06:39 +0530

    basic form from copilot

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <div>
       <h1 className="text-6xl text-center text-blue-500">Time Table</h1>
+      <button onClick={() => router.push("/getCredentials")}>Login</button>
     </div>
   );
 }
